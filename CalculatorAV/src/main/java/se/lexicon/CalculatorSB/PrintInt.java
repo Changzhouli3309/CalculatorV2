0c426d8d76541db46ca02bd06468cb44c173f47a
deltagare <deltagare@192.168.10.143>
2018-12-17 09:00:36 +0100

    up 2.07

--- CalculatorAV/src/main/java/se/lexicon/CalculatorSB/PrintInt.java
+++ CalculatorAV/src/main/java/se/lexicon/CalculatorSB/PrintInt.java
@@ -2,18 +2,13 @@ package se.lexicon.CalculatorSB;
 
 public class PrintInt {
 	public static String g(double n) {
-		String s = "0";
-		String a = "";
+		String s = "";
 		if((n*10)%10==0) {
-			s = "0"+(int)n;
+			s = ""+(int)n;
 		}else {
-			s = "0"+n;
+			s = ""+n;
 		}
-		for (int i=1;i<s.length();i++) {
-			a+=s.charAt(i);
-		}
-		
-		return a;
+		return s;
 	}
 
 }
